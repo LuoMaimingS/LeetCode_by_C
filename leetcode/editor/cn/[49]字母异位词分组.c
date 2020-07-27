9234febc23de27beded6966746ae580c0f4da8ca
lei yu <mp1833013@smail.nju.edu.cn>
2020-07-28 00:06:01 +0800

    Str.

---
+++ leetcode/editor/cn/[49]字母异位词分组.c
@@ -0,0 +1,35 @@
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。 
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+char *** groupAnagrams(char ** strs, int strsSize, int* returnSize, int** returnColumnSizes){
+
+}
+
+
+//leetcode submit region end(Prohibit modification and deletion)
